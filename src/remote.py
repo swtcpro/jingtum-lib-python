5485834b454ab7d225d82fca7bf40663181b08b3
tracy4262 <616958232@qq.com>
2018-05-29 17:34:29 +0800

    20180529提交代码

--- src/remote.py
+++ src/remote.py
@@ -9,9 +9,10 @@
  *   local_sign: false, // default sign tx in jingtumd
  * }
 """
+import json
 from inspect import isfunction
 
-from src.server import Server
+from src.server import Server, WebSocketServer
 from src.request import Request
 from src.utils.cache import LRUCache
 
@@ -22,18 +23,64 @@ class Remote:
     def __init__(self, options):
         self.opts = options
         self.local_sign = options.local_sign
-        self.server = Server(self)
+        self.server = WebSocketServer(self)
         self.status = {"ledger_index": 0}
         self.requests = {}
         self.cache = LRUCache(100)  # 100 size，为cache和path设置缓存
         self.path = LRUCache(2100)  # 2100 size
 
+    def connect(self, callback):
+        """
+        connect first on every case
+        :param callback:(error, result)
+        :return:
+        """
+        if self.server:
+            return callback('server not ready')
+        self.server.connect(callback)
+
+    def is_connected(self):
+        """
+        check is remote is connected to jingtumd
+        :return:
+        """
+        return self.server.connected
+
+    def handle_message(self, data):
+        # 此处可能要处理异常情况
+        data = json.loads(data)
+
+        if not data:
+            return
+        if (data.type == 'ledgerClosed'):
+            self.handle_ledger_closed(data)
+        elif (data.type == 'serverStatus'):
+            self.handle_server_status(data)
+        elif (data.type == 'response'):
+            self.handle_response(data)
+        elif (data.type == 'transaction'):
+            self.handle_transaction(data)
+        elif (data.type == 'path_find'):
+            self.handle_path_find(data)
+
     def submit(self, command, data, filter, callback):
+        """
+        request to server and backend
+        :param command:
+        :param data:
+        :param filter:
+        :param callback:
+        :return:
+        """
         if (isfunction(callback)):
             req_id = self.server.send_message(command, data)
             self.requests[req_id] = {
                 # 此处还有些代码逻辑没实现
                 # 根据command, data, filter, callback生成新的request对象
+                command: command,
+                data: data,
+                filter: filter,
+                callback: callback
             }
 
     # def subscribe(self, address, secret):
