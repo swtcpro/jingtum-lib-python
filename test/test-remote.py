b965a31f801162a2f798d839ee125ec17470d88d
caizhenglong <caizl2002@hotmail.com>
2018-07-20 15:58:54 +0800

    add testcase

--- test/test-remote.py
+++ test/test-remote.py
@@ -5,40 +5,29 @@
  * Time: 11:25
  * Description: 测试模块
 """
-import sys
+import unittest
 
 #from src.logger import logger
-
-sys.path.append("..\src")
 from src.remote import Remote
-from src.util import *
 
-def CheckErr(err, result):
-    if (err):
-        print('err:', err)
-    elif result:
-        print('res:', result)
+class RemoteTest(unittest.TestCase):
+    def test_create_pay_object(self):
+        remote = Remote()
+        if not isinstance(remote.connect(None), Exception):
+            tx = remote.buildPaymentTx({
+                'account': 'jB7rxgh43ncbTX4WeMoeadiGMfmfqY2xLZ',
+                'to': 'jDUjqoDZLhzx4DCf6pvSivjkjgtRESY62c',
+                'amount': {
+                    "value": 0.5,
+                    "currency": "SWT",
+                    "issuer": ""
+                }
+            })
+            tx.setSecret('sn37nYrQ6KPJvTFmaBYokS3FjXUWd')
+            tx.addMemo('给jDUjqoDZLhzx4DCf6pvSivjkjgtRESY62c支付0.5swt.')  # 可选
+            tx.addMemo('123')
+            s=tx.submit()
+            print('result is', s)
 
-
-def test(err, callback):
-    if err:
-        return print('err:', err)
-    tx = remote.buildPaymentTx({
-        'account': 'jB7rxgh43ncbTX4WeMoeadiGMfmfqY2xLZ',
-        'to': 'jDUjqoDZLhzx4DCf6pvSivjkjgtRESY62c',
-        'amount': {
-            "value": 0.5,
-            "currency": "SWT",
-            "issuer": ""
-        }
-    })
-    tx.setSecret('sn37nYrQ6KPJvTFmaBYokS3FjXUWd')
-    tx.addMemo('给jDUjqoDZLhzx4DCf6pvSivjkjgtRESY62c支付0.5swt.')  # 可选
-    tx.addMemo('123')
-    tx.submit(CheckErr)
-    #logger.info(callback)
-
-remote = Remote()
-result = remote.connect(None)
-#logger.info(result)
-test(None, None)
+if __name__ == '__main__':
+    unittest.main()
