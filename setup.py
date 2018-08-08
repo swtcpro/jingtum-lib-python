196439585facee3c3b61a2697e36b45635d74ea9
caizhenglong <caizl2002@hotmail.com>
2018-08-08 17:39:04 +0800

    fix some errors

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 from setuptools import setup
 
 setup(
-     name="jingtum_libs",
-     version="1.0.3",
+     name="jingtum_python_lib",
+     version="1.0.11",
      author="wuhan python dev team",
      author_email="caizl2002@hotmail.com",
      description=("jingtum_lib to be used for interacting with jingtum blockchain network"),
-     license="GPLv3",
-     keywords="jingtum",
+     license="MIT",
+     keywords=["jingtum", "lib", "python"],
      url="https://github.com/swtcpro/jingtum-lib-python",
-     packages=['jingtum_python_baselib', 'src','test', 'src.utils'],
-     data_files=[('', ['requirements.txt', 'LICENSE', 'README.md']),
-                 ('src', ['src/config.json'])],
+     packages=['jingtum_python_lib', 'jingtum_python_baselib'],
+     include_package_data=True,
+     package_data={'jingtum_python_lib': ['jingtum_python_lib\config.json']},
 
      install_requires=[
          'ecdsa >= 0.13',
@@ -25,10 +25,10 @@ setup(
      ],
 
      #long_description=open('README.md').read,
-     classifiers=[  # 程序的所属分类列表
-         "Development Status :: 3 - Alpha",
-         "Topic :: Utilities",
-         "License :: OSI Approved :: GNU General Public License (GPL)",
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
      ],
      # 此项需要，否则卸载时报windows error
      zip_safe=False
