f2f44414ecf1a90829d99e9ea329027c4c8634e4
caizhenglong <caizl2002@hotmail.com>
2018-08-16 08:51:04 +0800

    remove nouse para

--- test/test_41_413.py
+++ test/test_41_413.py
@@ -14,32 +14,32 @@ from jingtum_python_lib.remote import Remote
 class RemoteTest(unittest.TestCase):
     def test_conn(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             result = remote.get_connect_info()
             logger.info(result)
 
     def test_conn_close(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.disconnect()
 
     def test_server_info(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_server_info()
             result = remote.parse_server_info(req.submit())
             logger.info(result)
 
     def test_ledger_closed(self):
         remote = Remote(local_sign=True)
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_ledger_closed()
             result = remote.parse_ledger_closed(req.submit())
             logger.info(result)
 
     def test_ledger(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_ledger({'ledger_index': '943501','transactions': True})
             temp = req.submit()
             result = remote.parse_ledger(temp, req)
@@ -57,28 +57,28 @@ class RemoteTest(unittest.TestCase):
 
     def test_transaction(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_tx({'hash': '82C2C652431B63179E7CC775C3C1DBEC7AF1A6E91CC5A2671B195C19C76D701C'})
             result = remote.parse_transaction(req.submit())
             logger.info(result)
 
     def test_request_account_info(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_account_info({'account': 'jsMwaJ7EA4y7QgdvQzaD2CqzQQN4v7vLFK'})
             result = remote.parse_account_info(req.submit())
             logger.info(result)
 
     def test_request_account_tums(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_account_tums({'account': 'jEkffY8XtkEtkUKyYQT1Fv7MDp3grAtwdH'})
             result = remote.parse_account_tums(req.submit())
             logger.info(result)
 
     def test_request_account_relations(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_account_relations({'account': 'jEkffY8XtkEtkUKyYQT1Fv7MDp3grAtwdH',
                                                     'type': 'trust'})
             result = remote.parse_request_account_relations(req.submit())
@@ -96,25 +96,26 @@ class RemoteTest(unittest.TestCase):
 
     def test_request_account_offers(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_account_offers({'account': 'jJvkUPnBNQ4Wy5QGBKwMa8ftS5s9EDYXER'})
             result = remote.parse_request_account_offers(req.submit())
             logger.info(result)
 
     def test_request_account_tx(self):
         remote = Remote()
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_account_tx({'account': 'jsMwaJ7EA4y7QgdvQzaD2CqzQQN4v7vLFK',
-                                             'limit': 9})
+                                             'limit': 1})
             temp = req.submit()
             result = remote.parse_account_tx_info(temp, req)
             logger.info(result)
 
     def test_request_orderbook(self):
         remote = Remote(local_sign=True)
-        if not isinstance(remote.connect(None), Exception):
+        if not isinstance(remote.connect(), Exception):
             req = remote.request_order_book({'gets': {'currency': 'SWT', 'issuer': ''},
-                  'pays': {'currency': 'CNY', 'issuer': 'jBciDE8Q3uJjf111VeiUNM775AMKHEbBLS'}})
+                  'pays': {'currency': 'CNY', 'issuer': 'jBciDE8Q3uJjf111VeiUNM775AMKHEbBLS'},
+                 'limit':20})
             temp = req.submit()
             result = remote.parse_orderbook_info(temp)
             logger.info(result)
