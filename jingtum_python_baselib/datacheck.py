84a4ec84716ae6b0d38beb3c6fc9ec2bb2098730
caizhenglong <caizl2002@hotmail.com>
2018-11-13 15:16:38 +0800

    fit PEP8 rule

--- jingtum_python_baselib/datacheck.py
+++ jingtum_python_baselib/datacheck.py
@@ -5,9 +5,10 @@ CURRENCY_NAME_LEN = 3
 CURRENCY_NAME_LEN2 = 6
 TUM_NAME_LEN = 40
 
-#return True if the code is 3 letters/numbers
+
+# return True if the code is 3 letters/numbers
 def is_currency(in_code):
-    if isinstance(in_code,str):
+    if isinstance(in_code, str):
         if in_code and len(in_code) >= CURRENCY_NAME_LEN and len(in_code) <= CURRENCY_NAME_LEN2:
             return True
         else:
@@ -15,9 +16,9 @@ def is_currency(in_code):
     else:
         return False
 
-#Detect if the string contains only
-#numbers and capital letters.
 
+# Detect if the string contains only
+# numbers and capital letters.
 def is_letter_numer(in_str):
     numbers = '^[0-9A-Z]+$'
     if re.match(numbers, str(in_str)):
@@ -25,19 +26,21 @@ def is_letter_numer(in_str):
     else:
         return False
 
-#return True if the code is 40 letters/numbers
+
+# return True if the code is 40 letters/numbers
 def is_custom_tum(in_code):
-    if (is_letter_numer(in_code) and
-        len(str(in_code)) == TUM_NAME_LEN):
+    if is_letter_numer(in_code) and \
+    len(str(in_code)) == TUM_NAME_LEN:
         return True
     else:
         return False
 
+
 # Return True is the input string
 # is a valid TUM code
-#input must be defined and non-null
-#Make sure if the code meets the coding rule
-#tum: Custom tum, 40 capital letters or number
+# input must be defined and non-null
+# Make sure if the code meets the coding rule
+# tum: Custom tum, 40 capital letters or number
 def is_tum_code(in_code):
     return (isinstance(in_code,str) and \
         (in_code == 'SWT' or \
